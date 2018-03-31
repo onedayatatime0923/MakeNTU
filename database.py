c641587381c4fc1ccda3330f65ca02ea460a1970
kevin <onedayatatime0923@gmail.com>
2018-03-31 23:08:14 +0800

    jfkdj

--- database.py
+++ database.py
@@ -6,9 +6,9 @@ from translate  import Tag , Volcabulary
 formal = Tag('formal',['婚禮','典禮','正式','嚴肅'])
 casual = Tag('casual',['休閒','一般','普通','舒適','舒服'])
 sport = Tag('sport',['打球','運動','流汗','減肥','健身','球場','球隊','散步'])
-goodlooking = Tag('goodlooking',['好看','流行'])
+goodlooking = Tag('goodlooking',['好看','流行','女朋友','約會'])
 korean = Tag('korean',['韓系','韓流','歐爸','大衣'])
-warm = Tag('warm',['悶','熱','炎熱','太陽','不冷'])
+warm = Tag('warm',['悶','熱','好熱','炎熱','太陽','不冷'])
 cool = Tag('cool',['風','涼爽','清新','不熱'])
 cold = Tag('cold',['寒流','寒','冷','凍'])
 
@@ -24,8 +24,8 @@ dbClothes = {
     "球衣": [[22, 35], [sport, casual, warm, cool], ["球褲", "短褲", "長褲"], "球場"],
     "短襯衫": [[20, 35], [formal, casual, goodlooking, warm, cool], ["球褲", "短褲", "長褲"], "輕鬆卻不失打扮的場合"],
     "大衣圍巾": [[5, 18], [goodlooking, korean, cool, cold], ["長褲"], "需要打扮自我的流行場合"],
-    "吊嘎": [[25, 35], [casaul, sport, warm, cool], ["球褲", "短褲"], "很熱的天氣"],
-    "外套": [[5, 17], [casaul, goodlooking, cold], ["長褲"], "偏冷的天氣"],
+    "吊嘎": [[25, 35], [casual, sport, warm, cool], ["球褲", "短褲"], "很熱的天氣"],
+    "外套": [[5, 17], [casual, goodlooking, cold], ["長褲"], "偏冷的天氣"],
 }
 
 ixPants = ["球褲", "短褲", "長褲"]
@@ -40,17 +40,18 @@ dbPants = {
 ###################################################
 #            query                                #
 ###################################################
-vol= Volcabulary('./data/word2vec/zh.bin')
-vol.add_tag(formal)
-vol.add_tag(casual)
-vol.add_tag(sport)
-vol.add_tag(goodlooking)
-vol.add_tag(korean)
-vol.add_tag(warm)
-vol.add_tag(cool)
-vol.add_tag(cold)
+if __name__ == '__main__':
+    vol= Volcabulary('./data/word2vec/zh.bin')
+    vol.add_tag(formal)
+    vol.add_tag(casual)
+    vol.add_tag(sport)
+    vol.add_tag(goodlooking)
+    vol.add_tag(korean)
+    vol.add_tag(warm)
+    vol.add_tag(cool)
+    vol.add_tag(cold)
 
-with os.popen('python2 speech_to_text.py '+sys.argv[1]) as pse:
-    for s in pse:
-        statement=s
-print(statement,vol.query(statement))
+    with os.popen('python2 speech_to_text.py '+sys.argv[1]) as pse:
+        for s in pse:
+            statement=s
+    print(statement,vol.query(statement))
---
+++ drive.py
@@ -0,0 +1,30 @@
+
+# Include the Dropbox SDK
+import dropbox
+import requests
+import os
+assert dropbox
+class RemoteManager:
+    def __init__(self):
+        self.access_token='i7l8kexaJPAAAAAAAAABtaE_9wmLol-0Wqh3jY0p2_V3YoCTgagLHzwZ3OJXqEUy'
+        self.dbx = dropbox.Dropbox(self.access_token)
+        self.dbx.users_get_current_account()
+        self.url = "https://webdb-199518.appspot.com/"
+        self.GET_VALUE = "getvalue"
+    def ls(self,path=''):
+        return  self.dbx.files_list_folder(path).entries
+    def get(self,path,local_path='./'):
+        self.dbx.files_download_to_file(local_path,'/'+path)
+    def listen(self):
+        r = requests.post(self.url+self.GET_VALUE, {"tag":"Recorded"})
+        text = r.json()
+        req = bool(int(text[2][1]))
+        return req
+
+if __name__ == '__main__':
+    rm=RemoteManager()
+    while True:
+        if rm.listen()==True:
+            print('downloading')
+            rm.get('recording1.3gp','./data/record/record.3gp')
+            os.system('cd ./data/record/ && ffmpeg -y -i record.3gp -crf 18 record.wav' )
