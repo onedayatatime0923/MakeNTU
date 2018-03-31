37ace10da744f68c7e93a2eaecec97d080f6ab56
kevin <onedayatatime0923@gmail.com>
2018-04-01 06:03:48 +0800

    jk

--- peep_client.py
+++ peep_client.py
@@ -1,10 +1,8 @@
 import os 
 import sys
-from translate  import Tag , Volcabulary
-from database import formal,casual,sport,goodlooking,korean,warm,cool,cold
 from drive import RemoteManager
 import time
-assert Tag and sys and os
+assert sys and os
 
 
 
@@ -12,15 +10,6 @@ assert Tag and sys and os
 ###################################################
 #            query                                #
 ###################################################
-vol= Volcabulary('./data/word2vec/zh.bin')
-vol.add_tag(formal)
-vol.add_tag(casual)
-vol.add_tag(sport)
-vol.add_tag(goodlooking)
-vol.add_tag(korean)
-vol.add_tag(warm)
-vol.add_tag(cool)
-vol.add_tag(cold)
 rm=RemoteManager()
 
 while True:
