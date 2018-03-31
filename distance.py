9b70733038f2d18de06dfb824cc7eb3023d3ab7c
ChenTsaiShien <B04505025@ntu.edu.tw>
2018-03-31 15:32:07 +0800

    Add files via upload

--- distance.py
+++ distance.py
@@ -2,6 +2,8 @@ import time
 import RPi.GPIO as GPIO
 
 def distance():
+  dist = []
+
   # Use BCM GPIO references
   # instead of physical pin numbers
   GPIO.setmode(GPIO.BCM)
@@ -14,31 +16,34 @@ def distance():
   GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
   GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
 
-  # Set trigger to False (Low)
-  GPIO.output(GPIO_TRIGGER, False)
-
   # Allow module to settle
-  time.sleep(0.5)
+  time.sleep(0.2)
 
-  # Send 10us pulse to trigger
-  GPIO.output(GPIO_TRIGGER, True)
-  time.sleep(0.00001)
-  GPIO.output(GPIO_TRIGGER, False)
-  start = time.time()
-  while GPIO.input(GPIO_ECHO)==0:
+  for i in range(4):
+    # Set trigger to False (Low)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    # Send 10us pulse to trigger
+    GPIO.output(GPIO_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
     start = time.time()
+    while GPIO.input(GPIO_ECHO)==0:
+      start = time.time()
+  
+    while GPIO.input(GPIO_ECHO)==1:
+      stop = time.time()
 
-  while GPIO.input(GPIO_ECHO)==1:
-    stop = time.time()
-  GPIO.cleanup()
+    # Calculate pulse length
+    elapsed = stop-start
 
-  # Calculate pulse length
-  elapsed = stop-start
+    # Distance pulse travelled in that time is time
+    # multiplied by the speed of sound (cm/s)
+    distance = elapsed * 34000/2
+    dist.append(distance)
 
-  # Distance pulse travelled in that time is time
-  # multiplied by the speed of sound (cm/s)
-  distance = elapsed * 34000
-  return (distance/2)
+  GPIO.cleanup()  
+  return sum(dist)/len(dist)
 
 while True: 
-  print(str(distance()) + "cm")
+  print(str(distance()) + "    cm")
