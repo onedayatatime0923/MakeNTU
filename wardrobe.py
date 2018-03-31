fd402180e3d24f1b151b200e2dec64ac40c2427e
ChenTsaiShien <B04505025@ntu.edu.tw>
2018-04-01 06:23:55 +0800

    Add files via upload

--- wardrobe.py
+++ wardrobe.py
@@ -29,11 +29,12 @@ class Wardrobe:
             speak("這是件"+clothes+"，適合穿於"+self.ClothesInfo[kind].get_value(clothes, "Intro"))
         
     def findSpace(self, kind):
+        degree = self.degree[kind]
         canditate = [degree, (degree+60)%360, (degree+300)%360, (degree+120)%360, (degree+240)%360, (degree+180)%360]
         for i in range(6):
             if self.ClothesPos[kind][canditate[i]][0] == None:
                 return canditate[i]
-        return degree
+        return -1
 
     def moveto(self, kind, goal):
         print("Wardrobe of", kind, "has rotated from", self.degree[kind], " degree to", goal, "degree")
@@ -52,26 +53,41 @@ class Wardrobe:
                 break
                                
         clothesList = list(self.ClothesPos[0].values())
-        clothesList = list(set(list(filter(lambda a: a != None, clothesList))))
+        clothesList = [i[0] for i in clothesList]
+        clothesList = list(filter(lambda a: a != None, clothesList))
+        clothesList = list(set(clothesList))
         pantsList = list(self.ClothesPos[1].values())
-        pantsList = list(set(list(filter(lambda a: a != None, pantsList))))
+        pantsList = [i[0] for i in pantsList]
+        pantsList = list(filter(lambda a: a != None, pantsList))
+        pantsList = list(set(pantsList))
 
-        clothesDict = {}
-        pantsDict = {}
+        clothesKeep = []
+        pantsKeep = []
         for i in range(len(clothesList)):
-            clothesDict.update({clothesList[i], self.decideSuitable(clothesList[i], 0, feature)})
-        for i in range(len(pantsList)):
-            pantListDict.update({pantsList[i], self.decideSuitable(pantsList[i], 1, feature)})
-        if len(clothesDict) is 0:
+            if self.decideSuitable(clothesList[i], 0, feature):
+                clothesKeep.append(clothesList[i])
+        if len(clothesKeep) is 0:
             print("This wardrobe can not fit you, sorry")
+        for i in range(len(pantsList)):
+            if self.decideSuitable(pantsList[i], 1, feature):
+                pantsKeep.append(pantsList[i])
             
-        clothesData = (self.ClothesInfo[0].ix[clothesList])["Clothing"]
+        clothesData = (self.ClothesInfo[0].ix[clothesKeep])["Clothing"]
         for i in range(clothesData.size):
             for j in range(len(clothesData[i])):
-                if not (pantsDict(clothesData[i][j])):
+                if clothesData[i][j] not in pantsKeep:
                     clothesData[i][j] = ""
             clothesData[i] = list(filter(lambda a: a != "", clothesData[i]))
-        return clothesData
+        
+        finalList = []
+        for i in range(6):
+            if self.ClothesPos[0][60*i][0] in clothesData:
+                clothes = self.ClothesPos[0][60*i][0]
+                for j in range(6):
+                    if self.ClothesPos[1][60*j][0] in clothesData[clothes]:
+                        finalList.append((self.ClothesPos[0][60*i][1], self.ClothesPos[1][60*j][1]))
+        return finalList
+
 
     def decideSuitable(self, clothes, kind, ft):
         cft = (self.ClothesInfo[kind].loc[clothes])[1]
