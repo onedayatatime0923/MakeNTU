702d59fdbc1e46c13b18ea8cb96a55e789f635b4
kevin <onedayatatime0923@gmail.com>
2018-03-31 16:02:42 +0800

    dkfj

--- webcam.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-def takePhoto(filename):
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    cv2.imwrite(filename+".jpeg", frame)
-    cap.release()
-    cv2.destroyAllWindows()
