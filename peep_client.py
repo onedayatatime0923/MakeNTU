9de15b265aea0b498e1ca9dc464bd7a14698b981
kevin <onedayatatime0923@gmail.com>
2018-04-01 07:23:00 +0800

    dkfj

--- peep_client.py
+++ peep_client.py
@@ -20,7 +20,7 @@ while True:
         with open('score.txt','rb') as f:
             for s in f.readlines():
                 print(s)
-                s=str(s)[2,-3].strip('\n').split(',')
+                s=str(s)[2:-3].strip('\n').split(',')
                 score.append(tuple(s))
         print(score)
                 
