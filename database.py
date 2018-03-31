cac8b302d4942567eebf46867a601561d9bca23e
kevin <onedayatatime0923@gmail.com>
2018-03-31 13:13:40 +0800

    new file:   __pycache__/translate.cpython-35.pyc 	modified:   database.py 	modified:   translate.py

--- database.py
+++ database.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
+from translate  import Tag , Volcabulary
+
+formal=Tag('formal',['婚禮','典禮','正式','嚴肅'])
+casual=Tag('casual',['休閒','一般','普通','舒適','舒服'])
+sport=Tag('sport',['打球','運動','流汗','減肥','健身','球場','球隊','散步'])
+goodlooking=Tag('goodlooking',['好看','流行'])
+korean=Tag('korean',['韓系','韓流','歐爸','大衣'])
+warm=Tag('warm',['悶','熱','炎熱','太陽'])
+cool=Tag('cool',['風','涼爽','清新'])
+cold=Tag('cold',['寒流','寒冷','寒','凍'])
+
 
 featureTitle = [["TempRange", "Sun_Rain", "Occasion", "Clothing", "Intro"],
                 ["TempRange", "Sun_Rain", "Occasion", "Intro"]]
 
 ixClothes = ["西裝上衣", "球衣", "厚外套", "帽梯", "雨衣"]
 dbClothes = {
-    "西裝上衣": [[5, 35], "sunny", ["formal"], ["西裝褲"], "正式場合"],
-    "球衣": [[23, 35], "sunny", ["sport", "easy"], ["球褲", "短褲", "長褲"], "球場"],
-    "厚外套": [[5, 18], "sunny", ["goodlooking", "easy"], ["長褲"], "偏冷的天氣"],
-    "帽梯": [[7, 22], "sunny", ["goodlooking", "easy"], ["球褲", "短褲", "長褲"], "輕鬆卻不失打扮的場合"],
-    "雨衣": [[5, 35], "rainy", ["goodlooking", "easy"], ["雨褲"], "雨天"]
+    "西裝上衣": [[5, 35], "sunny", [formal], ["西裝褲"], "正式場合"],
+    "球衣": [[23, 35], "sunny", [sport, casual], ["球褲", "短褲", "長褲"], "球場"],
+    "短襯衫": [[23, 35], "sunny", [ casual,goodlooking], ["球褲", "短褲", "長褲"], "球場"],
+    "大衣圍巾": [[7, 22], "sunny", [korean, warm], ["球褲", "短褲", "長褲"], "輕鬆卻不失打扮的場合"],
+    "吊嘎": [[5, 35], "rainy", [warm], ["雨褲"], "雨天"],
+    "外套": [[5, 18], "sunny", [goodlooking, casual], ["長褲"], "偏冷的天氣"],
 }
 
 ixPants = ["西裝褲", "球褲", "短褲", "長褲", "雨褲"]
@@ -20,3 +32,26 @@ dbPants = {
     "長褲": [[5, 22], "sunny", ["easy"], "偏冷的天氣"],
     "雨褲": [[5, 35], "rainy", ["goodlooking", "easy"], "雨天"]
 }
+
+
+###################################################
+#            query                                #
+###################################################
+vol= Volcabulary('./data/word2vec/zh.bin')
+vol.add_tag(formal)
+vol.add_tag(casual)
+vol.add_tag(sport)
+vol.add_tag(goodlooking)
+vol.add_tag(korean)
+vol.add_tag(warm)
+vol.add_tag(cool)
+vol.add_tag(cold)
+
+statement='我想去打球'
+print(statement,vol.query(statement))
+statement='我今天要去找女友'
+print(statement,vol.query(statement))
+statement='今天我爸結婚'
+print(statement,vol.query(statement))
+statement='今天好悶，可是我想去約會'
+print(statement,vol.query(statement))
--- translate.py
+++ translate.py
@@ -3,7 +3,8 @@ from gensim.models import Word2Vec
 from gensim.models import KeyedVectors
 from sklearn.metrics.pairwise import cosine_similarity
 import jieba
-assert Word2Vec and KeyedVectors and jieba
+import numpy as np
+assert Word2Vec and KeyedVectors and jieba and cosine_similarity and np
 
 
 class Volcabulary:
@@ -12,30 +13,44 @@ class Volcabulary:
         self.w2v = Word2Vec.load(path)
         self.tag=[]
     def add_tag(self,text):
-        if text in self.w2v.wv.vocab:
-            self.tag.append(text)
-        else :
-            raise ValueError("tag is not availible.")
+        self.tag.append(text)
     def query(self,text):
         score={}
         for i in self.tag:
             score[i]=self.max_simularity(i,text)
-        return score
+        return sorted(score.items(), key=lambda x: x[1],reverse=True)
     def max_simularity(self,tag,text):
-        #text=list(jieba.cut(text))
-        score=[]
+        text=list(jieba.cut(text))
+        scores=[]
         for i in text:
-            if i in self.w2v.wv.vocab:
-                score.append(cosine_similarity(self.w2v[tag].reshape((1,-1)),self.w2v[i].reshape((1,-1)))[0,0])
-            else :
-                score.append(0)
-        print('text {}, tag {}, score {}'.format(text,tag,score))
-        return max(score)
+            for j in tag.feature:
+                if i in self.w2v.wv.vocab and j in self.w2v.wv.vocab:
+                    score=(cosine_similarity(self.w2v[j].reshape((1,-1)),self.w2v[i].reshape((1,-1)))[0,0])
+                    #score=np.linalg.norm(self.w2v[j]-self.w2v[i])
+                else :
+                    score=(1 if j==i else 0)
+                scores.append(score)
+        #print('text {}, tag {}, score {}'.format(text,tag,scores))
+        return max(scores)
+class Tag:
+    def __init__(self,name,feature):
+        self.feature=feature
+        self.name=name
+    def __str__(self):
+        return "{}".format(self.feature)
+    def __repr__(self):
+        return "{}".format(self.name)
+
 
 if __name__ == '__main__':
     vol= Volcabulary('./data/word2vec/zh.bin')
-    vol.add_tag('韓')
-    vol.add_tag('暖')
-    vol.add_tag('冷')
-    vol.add_tag('涼')
-    print(vol.query('今天風好大'))
+    vol.add_tag(Tag(['婚禮','喪禮','典禮','正式','嚴肅']))
+    vol.add_tag(Tag(['打球','運動','流汗','減肥','健身','球場','球隊']))
+    vol.add_tag(Tag(['休閒','一般','普通','舒適','舒服','散步','約會','好看','悶','','熱','悶熱','炎熱']))
+    vol.add_tag(Tag(['寒流','寒冷','寒','凍','保暖','保溫','韓系','韓流','約會','好看','正式','流行']))
+    vol.add_tag(Tag(['悶','熱','炎熱','休閒','健身','肌肉','曬黑','曬太陽']))
+    vol.add_tag(Tag(['寒流','寒冷','寒','凍','約會','好看','正式','流行','防風']))
+
+    print(vol.query('我想去打球'))
+    print(vol.query('我今天要去找女友'))
+    print(vol.query('今天我爸結婚'))
