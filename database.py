10c25a4d1bbd68a337f13f77d86ab6fd791cb1d7
kevin <onedayatatime0923@gmail.com>
2018-04-01 06:30:56 +0800

    dkfj

--- database.py
+++ database.py
@@ -24,8 +24,8 @@ dbClothes = {
     "球衣": [[22, 35], [sport, casual, warm, cool], ["球褲", "短褲", "長褲"], "球場"],
     "短襯衫": [[20, 35], [formal, casual, goodlooking, warm, cool], ["球褲", "短褲", "長褲"], "輕鬆卻不失打扮的場合"],
     "大衣圍巾": [[5, 18], [goodlooking, korean, cool, cold], ["長褲"], "需要打扮自我的流行場合"],
-    "吊嘎": [[25, 35], [casaul, sport, warm, cool], ["球褲", "短褲"], "很熱的天氣"],
-    "外套": [[5, 17], [casaul, goodlooking, cold], ["長褲"], "偏冷的天氣"],
+    "吊嘎": [[25, 35], [casual, sport, warm, cool], ["球褲", "短褲"], "很熱的天氣"],
+    "外套": [[5, 17], [casual, goodlooking, cold], ["長褲"], "偏冷的天氣"],
 }
 
 ixPants = ["球褲", "短褲", "長褲"]
